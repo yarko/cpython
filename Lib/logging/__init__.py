f8d3b244730c7b94f481e213c59059736f1a2dd3
Yarko Tymciurak <yarkot1@gmail.com>
2017-12-04 19:22:11 -0600

    attempt at fixing stream emits, where stream/C function requires unicode, but python2 default is POSIX/ASCII encoding

--- Lib/logging/__init__.py
+++ Lib/logging/__init__.py
@@ -870,25 +870,26 @@ class StreamHandler(Handler):
             fs = "%s\n"
             if not _unicode: #if no unicode support...
                 stream.write(fs % msg)
+            # TODO: yarkot: regardless of unicode support,
+            # if the message is or isn't an instance of unicode string,
+            #    it needs to be converted to unicode for StreamIO / StringIO
             else:
+                ufs = u'%s\n'
                 try:
-                    if (isinstance(msg, unicode) and
-                        getattr(stream, 'encoding', None)):
-                        ufs = u'%s\n'
-                        try:
-                            stream.write(ufs % msg)
-                        except UnicodeEncodeError:
-                            #Printing to terminals sometimes fails. For example,
-                            #with an encoding of 'cp1251', the above write will
-                            #work if written to a stream opened or wrapped by
-                            #the codecs module, but fail when writing to a
-                            #terminal even when the codepage is set to cp1251.
-                            #An extra encoding step seems to be needed.
-                            stream.write((ufs % msg).encode(stream.encoding))
-                    else:
-                        stream.write(fs % msg)
+                    stream.write(ufs % msg)
+                #except UnicodeEncodeError:
                 except UnicodeError:
-                    stream.write(fs % msg.encode("UTF-8"))
+                    #Printing to terminals sometimes fails. For example,
+                    #with an encoding of 'cp1251', the above write will
+                    #work if written to a stream opened or wrapped by
+                    #the codecs module, but fail when writing to a
+                    #terminal even when the codepage is set to cp1251.
+                    #An extra encoding step seems to be needed.
+                    try:
+                        stream.write((ufs % msg).encode(stream.encoding))
+                    except UnicodeError:
+                        stream.write(fs % msg.encode("UTF-8"))
+
             self.flush()
         except (KeyboardInterrupt, SystemExit):
             raise
